3f75c95aab3d3130bd7e77073dda81ab5fb8a6c7
Helena0504 <helenamaria.vargasq@gmail.com>
2025-08-26 18:47:22 -0600

    Update app.py

--- PO/PO/docker/web-spider/app/app.py
+++ PO/PO/docker/web-spider/app/app.py
@@ -8,8 +8,8 @@ from datetime import datetime
 import mariadb
 import sys
 
+#Parametros
 url_base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-
 
 #Parametros en la url
 db = "pubmed"
@@ -18,79 +18,66 @@ retstart = 0
 JOB_SIZE = 20
 retmax = JOB_SIZE
 
-#Datos a extraer
-count = 0
 
-
-new_url = f"{url_base}?db={db}&term={term}&retstart={retstart}&retmax={retmax}"
-#Enviamos un request
-response = requests.get(new_url)
-data = response.text #datos en formato xml
-datosFormatted = ET.fromstring(data)
-#Guardamos Count
-count = int(datosFormatted.find(".//Count").text)
-
-
-#Hacemos la conexión a MariaDB
-conn = mariadb.connect(
-    host=os.getenv('MARIADB'),
-    user=os.getenv('MARIADB_USER'),
-    password=os.getenv('MARIADB_PASS')
-)
-cursor = conn.cursor()
-
-# Crear la base de datos si no existe
-cursor.execute(f"CREATE DATABASE IF NOT EXISTS {os.getenv('MARIADB_DB')}")
-cursor.execute(f"USE {os.getenv('MARIADB_DB')}")
-
-# Crear la tabla si no existe
-cursor.execute(f"""
-CREATE TABLE IF NOT EXISTS {os.getenv('MARIADB_TABLE')} (
-    id VARCHAR(36) PRIMARY KEY,
-    estado VARCHAR(20),
-    lista_ids TEXT,
-    omitido TEXT,
-    fecha_inicio DATETIME,
-    fecha_final DATETIME
-)
-""")
-conn.commit()
-
-
-#Paginación
-while retstart < count:
-    lista_ids = []
-    
-    #Nueva url con parametros
+def obtener_count(url_base, db, term, retstart, retmax):
     new_url = f"{url_base}?db={db}&term={term}&retstart={retstart}&retmax={retmax}"
-
-    #Enviamos un request
+    # Enviamos un request
     response = requests.get(new_url)
-    data = response.text #datos en formato xml
+    data = response.text  # datos en formato xml
     datosFormatted = ET.fromstring(data)
+    # Guardamos Count
+    return int(datosFormatted.find(".//Count").text)
 
-    #Guardamos los datos
+
+
+def obtener_ids(url_base, db, term, retstart, retmax):
+    new_url = f"{url_base}?db={db}&term={term}&retstart={retstart}&retmax={retmax}"
+    # Enviamos un request
+    response = requests.get(new_url)
+    data = response.text  # datos en formato xml
+    datosFormatted = ET.fromstring(data)
+    # Guardamos los datos
+    lista_ids = []
     for elem in datosFormatted.findall(".//Id"):
         lista_ids.append(elem.text)
+    return lista_ids
 
-    #Creamos un job
-    job = {
-        "id": uuid.uuid4(), #libreria que genera id aleatorio y unico
-        "estado": "pending",
-        "lista_ids": lista_ids,
-        "omitido": [],
-        "fecha_inicio": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "fecha_final": None
-    }
 
-    #Aquí lo subimos a MariaDB
-    #Insertar Job
+
+def conectar_MariaDB():
+    conn = mariadb.connect(
+        host=os.getenv('MARIADB'),
+        user=os.getenv('MARIADB_USER'),
+        password=os.getenv('MARIADB_PASS')
+    )
+    cursor = conn.cursor()
+
+    # Crear la base de datos si no existe
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {os.getenv('MARIADB_DB')}")
+    cursor.execute(f"USE {os.getenv('MARIADB_DB')}")
+
+    # Crear la tabla si no existe
+    cursor.execute(f"""
+    CREATE TABLE IF NOT EXISTS {os.getenv('MARIADB_TABLE')} (
+        id VARCHAR(36) PRIMARY KEY,
+        estado VARCHAR(20),
+        lista_ids TEXT,
+        omitido TEXT,
+        fecha_inicio DATETIME,
+        fecha_final DATETIME
+    )
+    """)
+    conn.commit()
+    return conn, cursor
+
+
+
+def insertar_job(cursor, conn, table_name, job):
     insert_query = f"""
-        INSERT INTO {os.getenv('MARIADB_TABLE')}
+        INSERT INTO {table_name}
         (id, estado, lista_ids, omitido, fecha_inicio, fecha_final)
         VALUES (?, ?, ?, ?, ?, ?)
     """
-
     try:
         cursor.execute(insert_query, (
             str(job["id"]),
@@ -107,27 +94,51 @@ while retstart < count:
 
 
 
-    #Luego enviamos el id del job por RabbitMQ
-    DATA=os.getenv('DATAFROMK8S')
-    RABBIT_MQ=os.getenv('RABBITMQ')
-    RABBIT_MQ_PASSWORD=os.getenv('RABBITMQ_PASS')
-    QUEUE_NAME=os.getenv('RABBITMQ_QUEUE')
-
-    hostname = os.getenv('HOSTNAME')
+def enviar_rabbitmq(job_id):
+    """Luego enviamos el id del job por RabbitMQ"""
+    RABBIT_MQ = os.getenv('RABBITMQ')
+    RABBIT_MQ_PASSWORD = os.getenv('RABBITMQ_PASS')
+    QUEUE_NAME = os.getenv('RABBITMQ_QUEUE')
 
     credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
-    parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials) 
+    parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE_NAME)
 
-    msg = str(job["id"])
+    msg = str(job_id)
     channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=msg)
     time.sleep(1)
     connection.close()
-    ###########
 
-    retstart+=retmax
+
+
+
+
+
+#Ciclo Principal con Paginacion
+conn, cursor = conectar_MariaDB()
+table_name = os.getenv('MARIADB_TABLE')
+count = obtener_count(url_base, db, term, retstart, retmax)
+
+# Paginación
+while retstart < count:
+    lista_ids = obtener_ids(url_base, db, term, retstart, retmax)
+
+    # Creamos un job
+    job = {
+        "id": uuid.uuid4(),  
+        "estado": "pending",
+        "lista_ids": lista_ids,
+        "omitido": [],
+        "fecha_inicio": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "fecha_final": None
+    }
+
+    insertar_job(cursor, conn, table_name, job)
+    enviar_rabbitmq(job["id"])
+
+    retstart += retmax
 
 cursor.close()
 conn.close()
