d60c2370eb8640cbf762a8a5af29b6a7248bb9fa
priromerobarquero <d.romero@estudiantec.cr>
2025-08-31 15:43:32 -0600

    tests transformacion 2 y 3

--- PO/docker/spark-job/app/test.scala
+++ PO/docker/spark-job/app/test.scala
@@ -31,16 +31,44 @@ try {
     val createdDates = spark.sql("SELECT * FROM dates")
     // Filtra filas donde DOI sea "10.1126/science.adz6436"
     val dateValues = createdDates.filter(col("DOI") === "10.1126/science.adz6436")
-    assert(dateValues.count() == 1, "No se encontró exactamente un registro con DOI '10.1126/science.ady0241'")
+    assert(dateValues.count() == 1, "No se encontró exactamente un registro con DOI '10.1126/science.adz6436'")
     
     // Verifica que las fechas coincidan con los valores esperados de "08-02-2025" en indexed.date
-    val invalidindexed = dateValues.filter(col("indexed.date") =!= "08-02-2025")
+    val invalidindexed = dateValues.filter(col("indexed.date") =!= "08-025")
     assert(invalidindexed.count() == 0, "La fecha 'indexed.date' no coincide con el valor esperado")
     // Verifica que las fechas coincidan con los valores esperados de "08-01-2025" en created.date
     val invalidCreated = dateValues.filter(col("created.date") =!= "07-31-2025")
     assert(invalidCreated.count() == 0, "La fecha 'created.date' no coincide con el valor esperado")
 
+    //-----------------TRANSFORMACION 2-----------------
+    //Validación de extracción de autores de la vista temporal 
+    Functions.extractAuthors(spark)
+    val createdAuthors = spark.sql("SELECT * FROM autors") 
     
+    // Filtra filas donde DOI sea "10.1126/science.adz6436"
+    val autorValues = createdAuthors.filter(col("DOI") === "10.1126/science.adz6436")
+    assert(autorValues.count() == 1, "No se encontró exactamente un registro con DOI '10.1126/science.adz6436'")
+
+    // Verifica que los autores coincidan con la lista esperada
+    val autores = Array("Permar , Sallie R.", "Wilson , Patrick C.") 
+    val invalidAuthors = autorValues.filter(
+    sort_array(col("autor_names")) =!= sort_array(array(autores.map(lit): _*)))
+    assert(invalidAuthors.count() == 0, "La lista de 'autor_names' no coincide con el valor esperado")
+
+    //-----------------TRANSFORMACION 3-----------------
+    //Validación de extracción de titulos de la vista temporal 
+    Functions.extractTitlesAndReferences(spark)
+    val createdTitles = spark.sql("SELECT * FROM referencess") 
+    
+    // Filtra filas donde DOI sea "10.1126/science.adz6436"
+    val tittleValues = createdTitles.filter(col("DOIorg") === "10.1126/science.adz6436")
+    assert(tittleValues.count() == 1, "No se encontró exactamente un registro con DOI '10.1126/science.adz6436'")
+
+    // Verifica que los titulos referenciados coincidan con la lista esperada
+    val titulos = Array("Strategies for HIV-1 vaccines that induce broadly neutralizing antibodies","Precise targeting of HIV broadly neutralizing antibody precursors in humans","Vaccination with mRNA-encoded nanoparticles drives early maturation of HIV bnAb precursors in humans")
+    val invalidtittles = tittleValues.filter(
+    sort_array(col("reference_tittle")) =!= sort_array(array(titulos.map(lit): _*)))
+    assert(invalidtittles.count() == 0, "La lista de 'message.reference_tittle' no coincide con el valor esperado")
 
     sys.exit(0) // Salida 0 = tests OK
 
