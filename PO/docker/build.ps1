5a9726f6cb00fed8236318bb717797d5a45754b3
Carol Araya <carayacn@gmail.com>
2025-09-04 20:31:16 -0600

    Reparacion de BUG en conexion con funciones de spark

--- PO/docker/build.ps1
+++ PO/docker/build.ps1
@@ -3,15 +3,9 @@
 
 docker login
 
-Set-Location downloader
-docker build -t "$($args[0])/downloader" .
-docker push "$($args[0])/downloader"
 
-Set-Location ../web-spider
-docker build -t "$($args[0])/web-spider" .
-docker push "$($args[0])/web-spider"
 
-Set-Location ../spark-job
+Set-Location spark-job
 docker build -t "$($args[0])/spark-job" .
 docker push "$($args[0])/spark-job"
 
