2ca2d6847a89b6c5bfb9a148cbb07dfc98f908fa
Carol Araya <carayacn@gmail.com>
2025-09-08 10:10:16 -0600

    Imagenes de chromadb

--- TC1/Docker/build.ps1
+++ TC1/Docker/build.ps1
@@ -15,5 +15,29 @@ docker build -t "$Username/flask-example" .
 # Subir la imagen
 docker push "$Username/flask-example"
 
-# Volver a la carpeta anterior
-Set-Location ..
+#-------------------------CHROMADB ----------------------------
+# Ir a la carpeta Chroma-Redis
+Set-Location ../Chroma-Redis
+# Construir la imagen
+docker build -t "$Username/chroma-redis" .
+# Subir la imagen
+docker push "$Username/chroma-redis"
+
+#Carperta de Chroma-Memcached
+Set-Location ../Chroma-Memcached
+# Construir la imagen
+docker build -t "$Username/chroma-memcached" .
+# Subir la imagen
+docker push "$Username/chroma-memcached"
+
+#Carperta de ChromaDB
+Set-Location ../ChromaDB
+# Construir la imagen
+docker build -t "$Username/chromadb" .
+# Subir la imagen
+docker push "$Username/chromadb"
+
+#-------------------------FIN CHROMADB ----------------------------
+
+# Volver a la carpeta inicial
+Set-Location ../..
