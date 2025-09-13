a25ff6aaca6949920b69a6e947b1279efc623f92
Helena0504 <helenamaria.vargasq@gmail.com>
2025-09-12 19:03:44 -0600

    Métrica peticiones http

--- TC1/Docker/FlaskApp/app/app.py
+++ TC1/Docker/FlaskApp/app/app.py
@@ -15,7 +15,10 @@ cache_miss = Counter('total_cache_miss', 'Total Cache Miss', ['bd', 'cache'])
 
 
 #Suma peticiones http
-
+@app.after_request
+def diferencia_tiempo(response):
+    peticiones_http = Counter('total_peticiones_http', 'Total peticiones HTTP', ['bd', 'cache']).inc()
+    return response
 
 
 #Promedio de latencia
@@ -27,6 +30,8 @@ def iniciar_cronometro():
 #Se calcula el tiempo justo despues de la peticion
 @app.after_request
 def diferencia_tiempo(response):
-    tiempo = time.time() - request.start_time
+    tiempo = time.time() - request.start_time #tiempo tardado
+    promedio_tiempo = Histogram('promedio_tiempo_constlta', 'Total Cache Hit', ['bd', 'cache']).observe(tiempo)
     return response
+
 
