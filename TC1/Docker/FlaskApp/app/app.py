3c1a60b4044dcc09458bb59d5c4b8e1164f4ecf5
Helena0504 <helenamaria.vargasq@gmail.com>
2025-09-12 18:54:19 -0600

    Metrica latencia

--- TC1/Docker/FlaskApp/app/app.py
+++ TC1/Docker/FlaskApp/app/app.py
@@ -2,58 +2,31 @@ from flask import Flask, jsonify, request
 import os
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client import Counter, Histogram
+import time
 
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
 
 # Creación de distintas métricas
-peticiones_http = Counter('total_peticiones_http', 'Total peticiones HTTP', ['method', 'endpoint'])
-cache_hit = Histogram('promedio_tiempo_constlta', 'Total Cache Hit', ['method', 'endpoint'])
-cache_hit = Counter('total_cache_hit', 'Total Cache Hit', ['method', 'endpoint'])
-cache_miss = Counter('total_cache_miss', 'Total Cache Miss', ['method', 'endpoint'])
+peticiones_http = Counter('total_peticiones_http', 'Total peticiones HTTP', ['bd', 'cache'])
+promedio_tiempo = Histogram('promedio_tiempo_constlta', 'Total Cache Hit', ['bd', 'cache'])
+cache_hit = Counter('total_cache_hit', 'Total Cache Hit', ['bd', 'cache'])
+cache_miss = Counter('total_cache_miss', 'Total Cache Miss', ['bd', 'cache'])
 
 
-CROMA_MEMCACHED = "http://croma-memcached:5000"
-CROMA_REDIS     = "http://croma-redis:5000"
-ELASTICSEARCH_MEMCACHED = "http://elasticsearch-memcached:5000"
-ELASTICSEARCH_REDIS     = "http://elasticsearch-redis:5000"
-MARIADB_MEMCACHED = "http://mariadb-memcached:5000"
-MARIADB_REDIS     = "http://mariadb-redis:5000"
-POSTGRESQL_MEMCACHED = "http://mariadb-memcached:5000"
-POSTGRESQL_REDIS     = "http://mariadb-redis:5000"
+#Suma peticiones http
 
 
-@app.route("/")
-def hello_world():
-    DATA=os.getenv('PROMETHEUSENDPOINT')
-    return "<p>Hello, "+ DATA +"World!</p>"
 
-@app.route("/chromadb")
-def chroma():
-    client = chromadb.HttpClient(
-    host="databases-chromadb",  # nombre del servicio Kubernetes
-    port=8000
-    )
-    # Crear colección de prueba
-    collection = client.get_or_create_collection(name="coleccion_prueba2")
-    return jsonify({"coleccion": collection.name})
+#Promedio de latencia
+#Se inicia cronometro justo antes de la peticion para calcular latencia
+@app.before_request
+def iniciar_cronometro():
+    request.start_time = time.time()
 
-@app.route("/colecciones")
-def listar_colecciones():
-    client = chromadb.HttpClient(
-        host="databases-chromadb",
-        port=8000
-    )
-
-    # Obtener todas las colecciones
-    colecciones = client.list_collections()
-
-    # Extraer solo los nombres
-    nombres = [c.name for c in colecciones]
-
-    return jsonify({"colecciones": nombres})
-
-
+#Se calcula el tiempo justo despues de la peticion
+@app.after_request
+def diferencia_tiempo(response):
+    tiempo = time.time() - request.start_time
+    return response
 
