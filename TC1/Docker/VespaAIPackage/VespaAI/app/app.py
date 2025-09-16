270b97a54ba1f0a8909b9a9189c57c09f1810960
Helena0504 <helenamaria.vargasq@gmail.com>
2025-09-16 17:47:47 -0600

    Postgres y Vespa

--- TC1/Docker/VespaAIPackage/VespaAI/app/app.py
+++ TC1/Docker/VespaAIPackage/VespaAI/app/app.py
@@ -1,9 +1,36 @@
-from flask import Flask, jsonify
 import os
 from vespa.application import Vespa
+from flask import Flask, jsonify, request
+import time
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# --- MÉTRICAS ---
+peticiones_http = Counter('total_peticiones_http', 'Total peticiones HTTP', ['bd', 'cache'])
+promedio_tiempo = Histogram('promedio_tiempo_consulta', 'Tiempo promedio de consultas', ['bd', 'cache'])
+cache_hit = Counter('total_cache_hit', 'Total Cache Hit', ['bd', 'cache'])
+cache_miss = Counter('total_cache_miss', 'Total Cache Miss', ['bd', 'cache'])
+
+BD_TYPE = "vespaai"
+CACHE_TYPE = "none"
+
+@app.before_request
+def iniciar_tiempo():
+    request.start_time = time.time()
+    request.bd_type = BD_TYPE
+    request.cache_type = CACHE_TYPE
+
+@app.after_request
+def medir_peticiones(response):
+    tiempo = time.time() - request.start_time
+    promedio_tiempo.labels(bd=request.bd_type, cache=request.cache_type).observe(tiempo)
+    peticiones_http.labels(bd=request.bd_type, cache=request.cache_type).inc()
+    return response
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 
 @app.route('/health', methods=['GET'])
