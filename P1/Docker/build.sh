f93d21b39a845eb61904298f187e73fbe79739c4
Helena0504 <helenamaria.vargasq@gmail.com>
2025-09-29 18:52:34 -0600

    build.sh

--- P1/Docker/build.sh
+++ P1/Docker/build.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
-# $1 is the username
+USERNAME=$1
+
+# Login a Docker
 docker login
-# cd FlaskApp
-# docker build -t $1/flask-example .
-# docker push $1/flask-example
-cd ..
+
+# ---------------------------------------- Backend UI ----------------------------------------
+cd BackendUI || exit
+docker build -t "$USERNAME/backend-ui" .
+docker push "$USERNAME/backend-ui"
+
+# ---------------------------------------- UI ----------------------------------------
+cd ../FrontendUI || exit
+docker build -t "$USERNAME/frontend-ui" .
+docker push "$USERNAME/frontend-ui"
+
+# ---------------------------------------- Init MariaDB ----------------------------------------
+cd ../InitMariaDB || exit
+docker build -t "$USERNAME/init-mariadb" .
+docker push "$USERNAME/init-mariadb"
+
+# ---------------------------------------- Ingest CSV ----------------------------------------
+cd ../Ingest-CSV || exit
+docker build -t "$USERNAME/ingest-csv" .
+docker push "$USERNAME/ingest-csv"
+
+# ---------------------------------------- Ingest Parquet ----------------------------------------
+cd ../Ingest-Parket || exit
+docker build -t "$USERNAME/ingest-parket" .
+docker push "$USERNAME/ingest-parket"
+
+# ---------------------------------------- Spark HuggingFace ----------------------------------------
+cd ../HuggingFace || exit
+docker build -t "$USERNAME/huggingface" .
+docker push "$USERNAME/huggingface"
+
+# ---------------------------------------- Volver a la carpeta inicial ----------------------------------------
+cd ../..
