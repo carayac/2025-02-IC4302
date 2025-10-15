44b4009ef645f3e2b4d0af80941b4e48ea57c03c
prosoli <146582575+prosoli@users.noreply.github.com>
2025-10-14 23:54:14 -0600

    Imagen elastic-init en build.ps1

--- P1/Docker/build.ps1
+++ P1/Docker/build.ps1
@@ -12,35 +12,41 @@ docker build -t "$Username/backend-ui" .
 docker push "$Username/backend-ui"
 
 # #--------------------------------Backend UI Memcached----------------------------------------
-Set-Location ../BackendUI_Memcached
-docker build -t "$Username/backend-ui-memcached" .
-docker push "$Username/backend-ui-memcached"
+#  Set-Location ../BackendUI_Memcached
+#  docker build -t "$Username/backend-ui-memcached" .
+#  docker push "$Username/backend-ui-memcached"
 
 
 #----------------------------------------UI ----------------------------------------
 Set-Location ../FrontendUI
 docker build -t "$Username/frontend-ui" .
 docker push "$Username/frontend-ui"
-# # #----------------------------------------S3 Crawler----------------------------------------
-Set-Location ../S3_Crawler
-docker build -t "$Username/s3-crawler" .
-docker push "$Username/s3-crawler"
 
-#----------------------------------------Ingest----------------------------------------
+# #----------------------------------------S3 Crawler----------------------------------------
+#  Set-Location ../S3_Crawler
+#  docker build -t "$Username/s3-crawler" .
+#  docker push "$Username/s3-crawler"
 
-Set-Location ../Ingest-CSV
-docker build -t "$Username/ingest-csv" .
-docker push "$Username/ingest-csv"
+# # #----------------------------------------Ingest----------------------------------------
 
-Set-Location ../Ingest-Parket
-docker build -t "$Username/ingest-parket" .
-docker push "$Username/ingest-parket" 
+#  Set-Location ../Ingest-CSV
+#  docker build -t "$Username/ingest-csv" .
+#  docker push "$Username/ingest-csv"
+
+#  Set-Location ../Ingest-Parket
+#  docker build -t "$Username/ingest-parket" .
+#  docker push "$Username/ingest-parket" 
 
 
 # #----------------------------------------Spark Huggingface----------------------------------------
 # Set-Location ../HuggingFace
 # docker build -t "$Username/huggingface" .
 # docker push "$Username/huggingface"
+
+#  #----------------------------------------Elastic----------------------------------------
+#  Set-Location ../Elastic-Init
+#  docker build -t "$Username/elastic-init" .
+#  docker push "$Username/elastic-init"
 
 # Volver a la carpeta inicial
 Set-Location ../..
