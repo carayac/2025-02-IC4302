7574a3672f61641692f3596bb3a2b9948a9380b7
cjimenez0708 <chrisjimenezg08@gmail.com>
2025-11-29 16:16:37 -0600

    fixing horario neo

--- TC2/helm/backups/scripts/neo4j.sh
+++ TC2/helm/backups/scripts/neo4j.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 set -e
 
-DATE=$(date '+%Y%m%d%H%M')
-BACKUP_DIR="/neo4jdump/$DATE"
 
+DB_DIR="/data/data/databases/neo4j"
+
+if [ ! -d "$DB_DIR" ]; then
+  echo "La ruta $DB_DIR no existe. No se puede crear el backup."
+  exit 1
+fi
+
+DATE=$(date '+%Y%m%d%H%M')
+BACKUP_DIR="/backup/$DATE"
 mkdir -p "$BACKUP_DIR"
 
-neo4j-admin database dump neo4j \
-  --to-path="$BACKUP_DIR" \
-  --overwrite-destination=true
+neo4j stop || true
+
+neo4j-admin database dump neo4j --to-path="$BACKUP_DIR" --overwrite-destination=true
 
 gzip "$BACKUP_DIR/neo4j.dump"
 
-aws s3 cp "$BACKUP_DIR/neo4j.dump.gz" \
-  "s3://$BUCKET_NAME/$BACKUP_PATH/${DATE}.gz"
+echo "Subiendo a S3..."
+aws s3 cp "$BACKUP_DIR/neo4j.dump.gz" "s3://$BUCKET_NAME/$BACKUP_PATH/${DATE}.gz"
 
 echo "Backup completado exitosamente."
+
+neo4j start || true
+
+
+
+
+
+
+
+
+
+
 
 
--- TC2/helm/backups/scripts/neo4j_res.sh
+++ TC2/helm/backups/scripts/neo4j_res.sh
@@ -7,9 +7,14 @@ aws s3 cp s3://$BUCKET_NAME/$BACKUP_PATH/${BACKUP_NAME}.gz /restore/${BACKUP_NAM
 
 gunzip /restore/${BACKUP_NAME}.gz
 
+neo4j stop || true
+
 neo4j-admin database load neo4j \
   --from-path=/restore \
   --overwrite-destination=true
 
+neo4j start
+
 echo "Restauración completada exitosamente."
+
 
