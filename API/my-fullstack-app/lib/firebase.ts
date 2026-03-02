b2addd10fcabfedf47f2133a883adc9492484506
Daniel Arce Campos <163947407+DArceCampos@users.noreply.github.com>
2026-03-01 20:18:16 -0600

    Update Firebase config
    Replaced Firebase configuration credentials with placeholders.

--- API/my-fullstack-app/lib/firebase.ts
+++ API/my-fullstack-app/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 
 // Configuración de credenciales de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBGqepgUpRN9jfTfRnzSMiFOnXEEzSJ8VU",
-  authDomain: "proyect-db-2-itcr.firebaseapp.com",
-  projectId: "proyect-db-2-itcr",
-  storageBucket: "proyect-db-2-itcr.firebasestorage.app",
-  messagingSenderId: "121361936291",
-  appId: "1:121361936291:web:c0d99e83a723641fa3b416",
-  measurementId: "G-DZ3Q95442D",
+  apiKey: "apikey",
+  authDomain: "authdomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "G-measurementId",
 }
 
 // Se inicializa la app de Firebase para usar servicios de autenticación y de la base de datos
